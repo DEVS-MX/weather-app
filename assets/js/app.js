010df4964b02291441eca3b238d524b6f73dd18c
Ariel Loza <ariel@itnora.com>
2023-07-11 13:55:53 -0600

    Initialize the App

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+function getForecast() {
+const options = {
+  method: 'GET',
+  url: 'http://api.weatherapi.com/v1/forecast.json',
+  params: {
+    key: 'f2119a1539f146fd8ef90643231107',
+    q: 'London',
+    days: '1',
+    aqi: 'no',
+    alerts: 'no'
+  }
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+    const info = response.data;
+    const temperature = document.querySelector(".temperature");
+    const location = document.querySelector(".location");
+    const description = document.querySelector(".description");
+    temperature.innerText = info.current.temp_c + "°C";
+    location.innerText = info.location.name;
+    description.innerText = info.current.condition.text;
+}).catch(function (error) {
+  console.error(error);
+});
+}
+getForecast();
